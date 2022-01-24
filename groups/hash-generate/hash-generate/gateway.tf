9f9bbf35be4f3c277982d9728fc7bde36a5b53f6
Darren Smallwood <dsmallwood@companieshouse.gov.uk>
2022-01-24 12:20:08 +0000

    fix aws_api_gateway_method resource

--- groups/hash-generate/hash-generate/gateway.tf
+++ groups/hash-generate/hash-generate/gateway.tf
@@ -15,7 +15,7 @@ resource "aws_api_gateway_resource" "hash_generate_resource" {
 
 resource "aws_api_gateway_method" "hash_generate_method" {
   rest_api_id   = aws_api_gateway_rest_api.hash_generate.id
-  resource_id   = aws_api_gateway_resource.hash_generate.id
+  resource_id   = aws_api_gateway_resource.hash_generate_resource.id
   http_method   = "POST"
   authorization = "NONE"
   api_key_required = true
