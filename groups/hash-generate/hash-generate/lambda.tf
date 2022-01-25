75daac30f976d4feddb81b1288a581d36bd342db
Darren Smallwood <dsmallwood@companieshouse.gov.uk>
2022-01-25 16:08:07 +0000

    remove log policy

--- groups/hash-generate/hash-generate/lambda.tf
+++ groups/hash-generate/hash-generate/lambda.tf
@@ -52,21 +52,6 @@ data "aws_iam_policy_document" "lambda_trust" {
       ]
     }
   }
-
-  statement {
-
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = [
-      "arn:aws:logs:*:*:*"
-    ]
-  }
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
