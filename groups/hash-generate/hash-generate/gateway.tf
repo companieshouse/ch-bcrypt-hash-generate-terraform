0baba0d92b9970d60c818902c9fbfbed207686d0
Darren Smallwood <dsmallwood@companieshouse.gov.uk>
2022-01-26 10:16:31 +0000

    add config back in

--- groups/hash-generate/hash-generate/gateway.tf
+++ groups/hash-generate/hash-generate/gateway.tf
@@ -2,9 +2,9 @@ resource "aws_api_gateway_rest_api" "hash_generate" {
   name = "${var.service}-${var.environment}"
   description = "API gateway for the bcrypt hash generate service"
 
-  # endpoint_configuration {
-  #   types = ["REGIONAL"]
-  # }
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_resource" "hash_generate_resource" {
